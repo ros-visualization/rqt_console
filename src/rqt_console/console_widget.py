ad762854a4927e758713f94e446484d5479fcf35
Hirofumi Watanabe <hirofumi01.watanabe@g.softbank.co.jp>
2019-12-06 01:52:46 +0900

    [fix]handle_pause_clicked doesn't need args (#15)
    __Summary__
      handle_pause_clicked doesn't have arg, but set.
    * Change files
      modified:   src/rqt_console/console_widget.py

--- src/rqt_console/console_widget.py
+++ src/rqt_console/console_widget.py
@@ -694,7 +694,7 @@ class ConsoleWidget(QWidget):
             if messages:
                 self._model.insert_rows(messages)
 
-                self._handle_pause_clicked(True)
+                self._handle_pause_clicked()
 
             return True
 
