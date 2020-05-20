f0ed1043aafa691827985b6864cb6237b6bb97a3
Shane Loretz <sloretz@osrfoundation.org>
2020-05-20 11:19:16 -0700

    Use catkin_install_python()
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['rqt_console', 'rqt_console.filters'],
-    package_dir={'': 'src'},
-    scripts=['scripts/rqt_console']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
