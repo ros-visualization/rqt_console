ros-visualization/rqt_console

commit 9442cc8b5fb7166bc700ebb50ad4c48a300e174c

Changed the build type to ament_python and added setup.cfg (#21)
* Fixed package to run with ros2 run

Signed-off-by: ahcorde <ahcorde@gmail.com>

* Fix author and maintener

Signed-off-by: ahcorde <ahcorde@gmail.com>

* Fixed version on setup.py

Signed-off-by: ahcorde <ahcorde@gmail.com>

* Added feedback

Signed-off-by: ahcorde <ahcorde@gmail.com>

* Fixed description

Signed-off-by: ahcorde <ahcorde@gmail.com>

* package collected automatically using setuptools.find_packages()

Signed-off-by: ahcorde <ahcorde@gmail.com>

* Added feedback

Signed-off-by: ahcorde <ahcorde@gmail.com>

setup.py
@@ -1,0 +1,47 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'rqt_console'
+
+setup(
+    name=package_name,
+    version='1.1.1',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name + '/resource',
+            ['resource/console_settings_dialog.ui',
+             'resource/console_widget.ui',
+             'resource/text_browse_dialog.ui']),
+        ('share/' + package_name + '/resource/filters',
+            ['resource/filters/custom_filter_widget.ui',
+             'resource/filters/filter_wrapper_widget.ui',
+             'resource/filters/list_filter_widget.ui',
+             'resource/filters/text_filter_widget.ui',
+             'resource/filters/time_filter_widget.ui']),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['plugin.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Aaron Blasdel',
+    maintainer='Dirk Thomas, Aaron Blasdel',
+    maintainer_email='dthomas@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'rqt_console provides a GUI plugin for displaying and filtering ROS messages.'
+    ),
+    license='BSD',
+    entry_points={
+        'console_scripts': [
+            'rqt_console = ' + package_name + '.main:main',
+        ],
+    },
+)

CMakeLists.txt
@@ -1,23 +1,0 @@
-cmake_minimum_required(VERSION 3.5)
-project(rqt_console)
-
-# Load ament and all dependencies required for this package
-find_package(ament_cmake REQUIRED)
-find_package(ament_cmake_python REQUIRED)
-
-ament_python_install_package(${PROJECT_NAME}
-    PACKAGE_DIR src/${PROJECT_NAME})
-
-install(FILES plugin.xml
-  DESTINATION share/${PROJECT_NAME}
-)
-
-install(DIRECTORY resource
-  DESTINATION share/${PROJECT_NAME}
-)
-
-install(PROGRAMS scripts/rqt_console
-  DESTINATION lib/${PROJECT_NAME}
-)
-
-ament_package()

package.xml
@@ -12,8 +12,6 @@
   <url type="repository">https://github.com/ros-visualization/rqt_console</url>
   <url type="bugtracker">https://github.com/ros-visualization/rqt_console/issues</url>
 
-  <buildtool_depend>ament_cmake</buildtool_depend>
-
   <exec_depend>ament_index_python</exec_depend>
   <exec_depend version_gte="0.2.19">python_qt_binding</exec_depend>
   <exec_depend>rclpy</exec_depend>
@@ -28,8 +26,8 @@
   <exec_depend>rcl_interfaces</exec_depend>
 
   <export>
-    <!-- <architecture_independent/> -->
+    <architecture_independent/>
+    <build_type>ament_python</build_type>
     <rqt_gui plugin="${prefix}/plugin.xml"/>
-    <build_type>ament_cmake</build_type>
   </export>
 </package>

scripts/rqt_console
@@ -1,8 +1,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-from rqt_gui.main import Main
-
-main = Main()
-sys.exit(main.main(sys.argv, standalone='rqt_console.console.Console'))

setup.cfg
@@ -1,0 +1,4 @@
+[develop]
+script-dir=$base/lib/rqt_console
+[install]
+install-scripts=$base/lib/rqt_console

src/rqt_console/main.py
@@ -1,0 +1,12 @@
+import sys
+
+from rqt_gui.main import Main
+
+
+def main():
+    main = Main()
+    sys.exit(main.main(sys.argv, standalone='rqt_console.console.Console'))
+
+
+if __name__ == '__main__':
+    main()

commit 7c2996ca531175191d0befae8ce7fd6cb3b76753

Fix regression introduced in #21
Signed-off-by: ahcorde <ahcorde@gmail.com>

setup.py
@@ -1,4 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'rqt_console'
@@ -6,7 +5,8 @@ package_name = 'rqt_console'
 setup(
     name=package_name,
     version='1.1.1',
-    packages=find_packages(),
+    packages=[package_name, package_name + '.filters'],
+    package_dir={'': 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),