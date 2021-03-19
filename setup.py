dc498ee93df3f919574b967807e0235cf8f63d00
William Woodall <william@osrfoundation.org>
2021-03-18 18:14:57 -0700

    2.0.0
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_console'
 
 setup(
     name=package_name,
-    version='1.1.1',
+    version='2.0.0',
     packages=[package_name, package_name + '.filters'],
     package_dir={'': 'src'},
     data_files=[
