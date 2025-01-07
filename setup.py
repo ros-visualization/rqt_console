72ba867c39eceee93374d5484eac08bfb1e5b9b0
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-01-07 16:27:11 +0100

    2.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_console'
 
 setup(
     name=package_name,
-    version='2.3.0',
+    version='2.3.1',
     packages=[package_name, package_name + '.filters'],
     package_dir={'': 'src'},
     data_files=[
